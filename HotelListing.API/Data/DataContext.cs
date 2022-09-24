4690eaf7ae639e46b09a4a18dd034ae38496408c
cklasson <chrillec@gmail.com>
2022-09-24 21:26:15 +0200

    added migration after seeding data

--- HotelListing.API/Data/DataContext.cs
+++ HotelListing.API/Data/DataContext.cs
@@ -40,6 +40,34 @@ namespace HotelListing.API.Data
                   }
 
                 );
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   Id = 1,
+                   Name = "Hilton",
+                   Address = "Gatan 3",
+                   CountryId = 1,
+                   Rating = 3
+
+               },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Sheraton",
+                    Address = "Gatan 4",
+                    CountryId = 2,
+                    Rating = 3
+                },
+                 new Hotel
+                 {
+                     Id = 3,
+                     Name = "GB Glass",
+                     Address = "Gatan 4",
+                     CountryId = 3,
+                     Rating = 3
+                 }
+
+               );
         }
     }
 }
---
+++ HotelListing.API/Migrations/20220924192222_seeded_data.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class seeded_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Sweden", "SE" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "Norway", "NO" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Denmark", "DK" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1, "Gatan 3", 1, "Hilton", 3.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2, "Gatan 4", 2, "Sheraton", 3.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 3, "Gatan 4", 3, "GB Glass", 3.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
